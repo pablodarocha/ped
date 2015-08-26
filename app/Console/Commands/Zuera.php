da93a6e3eab3d99ce223fa64811cf826dfa63177
Pablo da Rocha <pablodarocha.ribeiro@gmail.com>
2015-08-26 12:52:16 -0300

    + first commit

---
+++ app/Console/Commands/Zuera.php
@@ -0,0 +1,69 @@
+<?php
+
+namespace App\Console\Commands;
+
+use Illuminate\Console\Command;
+use Symfony\Component\Console\Input\InputArgument;
+
+class Zuera extends Command
+{
+    /**
+     * The name and signature of the console command.
+     *
+     * @var string
+     */
+    protected $signature = 'zuera {name}';
+
+    /**
+     * The console command description.
+     *
+     * @var string
+     */
+    protected $description = 'Comando que exibe uma mensagem de Hello.';
+
+    /**
+     * Create a new command instance.
+     *
+     * @return void
+     */
+    public function __construct()
+    {
+        parent::__construct();
+    }
+
+    /**
+     * Execute the console command.
+     *
+     * @return mixed
+     */
+//    public function handle()
+//    {
+//        if ($name = $this->argument('name')) {
+//            $this->info('Hello {name}!');
+//
+//            return [
+//                ['name', InputArgument::OPTIONAL, 'O nome que deverá ser exibido.', 'World'],
+//            ];
+//        }
+//
+////        $name = $this->input->getArgument('name');
+////            $this->info('Hello {name}!');
+////
+////        return [
+////            ['name', InputArgument::OPTIONAL, 'O nome que deverá ser exibido.', 'World'],
+////        ];
+//    }
+
+    public function fire() {
+        $name = $this->input->getArgument("name");
+
+        $this->info("Hello {$name}!");
+    }
+
+    protected function getArguments()
+    {
+        return [
+            ['name', InputArgument::OPTIONAL, 'O nome que deverá ser exibido.', 'World'],
+        ];
+    }
+}
---
+++ config/services.php
@@ -0,0 +1,38 @@
+<?php
+
+return [
+
+    /*
+    |--------------------------------------------------------------------------
+    | Third Party Services
+    |--------------------------------------------------------------------------
+    |
+    | This file is for storing the credentials for third party services such
+    | as Stripe, Mailgun, Mandrill, and others. This file provides a sane
+    | default location for this type of information, allowing packages
+    | to have a conventional place to find your various credentials.
+    |
+    */
+
+    'mailgun' => [
+        'domain' => 'sandbox66c8ed11ac0c4366a41aaa523068aea2.mailgun.org',
+        'secret' => 'key-f307435d6cb4cd6be2941b015745b879',
+    ],
+
+    'mandrill' => [
+        'secret' => '',
+    ],
+
+    'ses' => [
+        'key'    => '',
+        'secret' => '',
+        'region' => 'us-east-1',
+    ],
+
+    'stripe' => [
+        'model'  => App\User::class,
+        'key'    => '',
+        'secret' => '',
+    ],
+
+];
